b93ec63020719cbfa333f9e14056000501672238
Egor Kosolapov <greezzly7@gmail.com>
2014-06-02 11:39:53 +0400

    crm db FIX 2

--- application/views/auth/email/forgot_password.tpl.php
+++ application/views/auth/email/forgot_password.tpl.php
@@ -1,6 +1,6 @@
 <html>
 <body>
 	<h1><?php echo sprintf(lang('email_forgot_password_heading'), $identity);?></h1>
-	<p>{unwrap}<a href='http://water.loc/auth/reset_password/<?php echo $forgotten_password_code?>'>Сбросить пароль</a>{/unwrap}</p>
+	<p>{unwrap}<a href='http://lineofhealth.ru/auth/reset_password/<?php echo $forgotten_password_code?>'>Сбросить пароль</a>{/unwrap}</p>
 </body>
 </html>
